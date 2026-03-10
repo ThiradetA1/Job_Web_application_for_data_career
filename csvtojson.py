087b283a83e6ab03eb6e0e5157ac4d9e98ef6c65
ThiradetA1 <teearm7777@gmail.com>
2026-03-10 21:50:05 +0700

    first

---
+++ csvtojson.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+csv_file_path = 'retail_sales_dataset.csv'
+json_file_path = 'data.json'
+
+df = pd.read_csv(csv_file_path)
+
+df.to_json(json_file_path, orient='records', indent=4)
+
+print(f"Successfully converted {csv_file_path} to {json_file_path} using pandas")
